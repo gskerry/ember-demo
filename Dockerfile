ebad1e6d6f223afb54eb2cfffc2a08624b1d8bfa
gskerry <gregory.skerry@gmail.com>
2017-11-19 20:46:59 -0800

    Expose and map livereload server port. (Not clear if necessary, load fail appeared to be due to ember template not supporting blank page)

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ WORKDIR /home/myapp
 RUN yarn global add ember-cli
 
 EXPOSE 4200
+EXPOSE 7020
 
 CMD [ "echo", "Welcome to Ember." ]
--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name ember_container -p 4200:4200 -v $(pwd):/home/myapp -w /home/myapp ember-proj_img:0.1.0 /bin/sh
+docker run -it --rm --name ember_container -p 4200:4200 -p 7020:7020 -v $(pwd):/home/myapp -w /home/myapp ember-proj_img:0.1.0 /bin/sh
