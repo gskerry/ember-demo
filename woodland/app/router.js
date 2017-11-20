1600a06d4dfe3a129e46f4c9480deee0da796a68
gskerry <gregory.skerry@gmail.com>
2017-11-19 22:08:56 -0800

    route, model and template for order detail

--- woodland/app/router.js
+++ woodland/app/router.js
@@ -7,19 +7,8 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-    // this.route('index', {path: '/'}) // Auto/implied
     this.route('orders', {path: '/orders'})
-    // this.route('orders') // Shorthand also works when template and route have same name
-    // Note, this also does not actually mention the template orders...
-        // it is a *URL path and an application route
+    this.route('order', {path: '/orders/:order_id'})
 });
 
 export default Router;
-
-
-// N.B.
-// $ ember generate route <name>
-    // adds this.routh(...) above
-    // adds a <name>.js to routes/
-    // adds a <name>.hbs to templates/
-// **
