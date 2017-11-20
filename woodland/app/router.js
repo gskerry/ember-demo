9115973fa1474c3b10b2c787023e254ce4f14721
gskerry <gregory.skerry@gmail.com>
2017-11-19 22:24:23 -0800

    refactor nested routes: order child of orders

--- woodland/app/router.js
+++ woodland/app/router.js
@@ -7,8 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-    this.route('orders', {path: '/orders'})
-    this.route('order', {path: '/orders/:order_id'})
+    this.route('orders', {path: '/orders'}, function(){
+        // this.route('order', {path: '/orders/:order_id'}
+        this.route('order', {path: '/:order_id'}) // note /orders can be removed (implied from nesting)
+    })
 });
 
 export default Router;
