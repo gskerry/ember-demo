bba09ca21d8ecce4c6b84007801138b6c1f546df
gskerry <gregory.skerry@gmail.com>
2017-11-19 21:55:46 -0800

    model with multiple data. Ember 'each' helper

--- woodland/app/routes/orders.js
+++ woodland/app/routes/orders.js
@@ -2,6 +2,16 @@ import Ember from 'ember'
 
 export default Ember.Route.extend({
     model(){
-        return 'Greg'
+        return [
+            {id: '1', name:'Jake'},
+            {id: '2', name:'Greg'}
+        ]
     }
+/*
+    model is primary
+    others:
+    activate(){}
+    deactivate(){}
+    redirect(){}
+*/
 })
