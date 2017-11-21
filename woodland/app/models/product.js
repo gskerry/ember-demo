1de291873f1eced0817b531a90f484895ac3eaf2
gskerry <gregory.skerry@gmail.com>
2017-11-20 17:27:35 -0800

    model scaffolding

---
+++ woodland/app/models/product.js
@@ -0,0 +1,15 @@
+import Ember from 'ember'
+
+export default Ember.Object.extend({
+
+})
+
+/*
+    let product = Product.create({
+        title: 'Sleeping Bag'
+    })
+
+    product.get('title') //=> 'Sleeping Bag'
+    product.set('title', 'Matches')
+    product.get('title') //=> 'Matches'
+*/
